d0efa8304d86e3d88472c70b34fc88447f9812c2
Liam Ruiz-Steblein <liamruiz@Liams-MacBook-Air-575.local>
2025-11-19 14:24:52 -0600

    task_12

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,6 @@
-function addNumbers() {
-  let n1 = Number(document.getElementById("num1").value);
-  let n2 = Number(document.getElementById("num2").value);
-  let sum = n1 + n2;
+function changeText() {
 
-  document.getElementById("result").textContent = "Result: " + sum;
+  document.getElementById("text").textContent = "You clicked the button!";
 }
 
-document.getElementById("addBtn").addEventListener("click", addNumbers);
+document.getElementById("addBtn").addEventListener("click", changeText);
